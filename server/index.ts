8ffec366a7b757d08adf2258e83848abc658ecb1
Namra Patel <patel4738@saskpolytech.ca>
2026-03-05 18:10:32 -0600

    changed the port number

--- server/index.ts
+++ server/index.ts
@@ -91,7 +91,7 @@ app.use((req, res, next) => {
   httpServer.listen(
     {
       port,
-      host: "0.0.0.0",
+      host: "127.0.0.1",
     },
     () => {
       log(`serving on port ${port}`);

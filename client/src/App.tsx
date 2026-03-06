dbdb20271586aa650d44b3ca55c78da870dc5239
Namra Patel <patel4738@saskpolytech.ca>
2026-03-05 19:15:44 -0600

    Added basename to the Wouter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -6,24 +6,29 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import OnePiece from "@/pages/OnePiece";
 
-function Router() {
+
+function AppRouter() {
   return (
-    <Switch>
-      <Route path="/" component={OnePiece} />
-      <Route component={NotFound} />
-    </Switch>
+    <Router base={import.meta.env.BASE_URL}>
+      <Switch>
+        <Route path="/" component={OnePiece} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
+
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router />
+        <AppRouter />
       </TooltipProvider>
     </QueryClientProvider>
   );
 }
+
 
 export default App;
